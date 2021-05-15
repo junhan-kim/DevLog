7fb7dcb654cb926137bd6730cb56900f6bcebaca
junhan-kim <kjune1236@gmail.com>
2021-05-16 00:22:35 +0900

    Add manage.py

--- DevLog-api-server/manage.py
+++ DevLog-api-server/manage.py
@@ -1,12 +1,36 @@
-from flask import Flask
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app.main import create_app, db
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 
-app = Flask(__name__)
+@manager.command
+def run():
+    app.run()
 
-@app.route('/')
-def hello_world():
-    return 'hello world!'
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    manager.run()
