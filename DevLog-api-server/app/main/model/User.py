953ea7361e48f6f49bbb92a1e577a687d3fa7270
junhan-kim <kjune1236@gmail.com>
2021-05-16 22:06:56 +0900

    Fix user model

--- DevLog-api-server/app/main/model/User.py
+++ DevLog-api-server/app/main/model/User.py
@@ -5,12 +5,12 @@ class User(db.Model):
     __tablename__ = "user_account"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100))
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
-    public_id = db.Column(db.String(100), unique=True)
+    public_id = db.Column(db.String(100), nullable=False, unique=True)
 
     @property
     def password(self):
