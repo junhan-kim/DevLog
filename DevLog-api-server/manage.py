3d8f0e974ca8528cdace64823d3ad620d90d085e
junhan-kim <kjune1236@gmail.com>
2021-05-17 02:30:32 +0900

    Add logger

--- DevLog-api-server/manage.py
+++ DevLog-api-server/manage.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import logging
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
@@ -8,20 +9,29 @@ from app import blueprint
 from app.main import create_app, db
 from app.main.model import user
 
+
+# app setting
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
-
 app.app_context().push()
-
 manager = Manager(app)
-
 migrate = Migrate(app, db)
-
 manager.add_command('db', MigrateCommand)
+
+# logger setting
+LOG_FORMATTER = logging.Formatter(
+    '%(asctime)s | %(levelname)s | Message : %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S')
+handler = logging.StreamHandler()
+handler.setFormatter(LOG_FORMATTER)
+logger = logging.getLogger("logger")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 @manager.command
 def run():
+    logger.info('App is running.')
     app.run(host='0.0.0.0', port=5000)
 
 
