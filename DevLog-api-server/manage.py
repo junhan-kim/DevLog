f41e3fe96873c75cffe62ef9043420b9a17dcff3
junhan-kim <kjune1236@gmail.com>
2021-05-19 02:18:51 +0900

    Add jwt functions

--- DevLog-api-server/manage.py
+++ DevLog-api-server/manage.py
@@ -8,6 +8,7 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app, db
 from app.main.model import user
+from app.main.model import blacklist
 
 
 # app setting
