3782c79c624200ec0ee1730dc1afbab0fe23691b
junhan-kim <kjune1236@gmail.com>
2021-05-22 03:41:13 +0900

    Add auth functions

--- DevLog-api-server/docker-entrypoint.sh
+++ DevLog-api-server/docker-entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py db init
 python manage.py db migrate --message 'initial database migration'
 python manage.py db upgrade
 
-# python manage.py run
-python manage.py test
+python manage.py run
+# python manage.py test
